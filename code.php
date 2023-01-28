b0c1260e9d0e7afe35016a8b23f592a040436be8
Gianluca Barranca <info@gianlucabarranca.it>
2023-01-28 15:57:34 +0100

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,11 @@
-		add_action('init', 'disattiva_gutenberg_per_tutto');
-			function disattiva_gutenberg_per_tutto() {
-				add_filter('use_block_editor_for_post_type', '__return_false');
-				add_filter('use_block_editor_for_post', '__return_false');
-				// Disables the block editor from managing widgets in the Gutenberg plugin.
-			add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
-			// Disables the block editor from managing widgets.
-			add_filter( 'use_widgets_block_editor', '__return_false' );
-			}
-			
+<?php
+add_action('init', 'disattiva_gutenberg_per_tutto');
+function disattiva_gutenberg_per_tutto() {
+	add_filter('use_block_editor_for_post_type', '__return_false');
+	add_filter('use_block_editor_for_post', '__return_false');
+// Disables the block editor from managing widgets in the Gutenberg plugin.
+	add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+// Disables the block editor from managing widgets.
+	add_filter( 'use_widgets_block_editor', '__return_false' );
+}
+?>
